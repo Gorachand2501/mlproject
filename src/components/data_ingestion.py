e8b9e6d52318d6a7b8f5021571b4169c8ecdd3b3
Gorachand2501 <gorachand2501@gmail.com>
2025-11-07 06:22:22 +0530

    data transformation pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,6 +2,8 @@ import os
 import sys
 from src.exception import CustomException
 from src.logger import logging
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -28,8 +30,8 @@ class DataIngestion:
 
         logging.info("Train-test split initiated")
         train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
-        df.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
-        df.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+        train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+        test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
         logging.info("Ingestion of data is completed")
 
         return(
@@ -39,5 +41,8 @@ class DataIngestion:
     
 if __name__=='__main__':
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
